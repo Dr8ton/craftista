c3a11e67d646b553d49dd1d8ae84f321dd77695d
Dr8ton <drayton.kittel@gmail.com>
2025-12-01 01:39:06 -0600

    added outputs so that I can know the ips.

---
+++ terraform/outputs.tf
@@ -0,0 +1,24 @@
+output "bastion_EIP" {
+  description = "The static IP of the Bastion server. "
+  value       = aws_eip.eip-bastion.public_ip
+}
+
+output "frontend_private_IP" {
+  description = "The internal IP of the Frontend server. "
+  value       = aws_instance.frontend.private_ip
+}
+
+output "voting_private_IP" {
+  description = "The internal IP of the Voting server. "
+  value       = aws_instance.voting.private_ip
+}
+
+output "recommendation_private_IP" {
+  description = "The internal IP of the Recommendation server. "
+  value       = aws_instance.recommendation.private_ip
+}
+
+output "catalogue_private_IP" {
+  description = "The internal IP of the Catalogue server. "
+  value       = aws_instance.catalogue.private_ip
+}
