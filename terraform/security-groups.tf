16496ddc364fc92b07ccecf1041d0361c4a1aa21
Dr8ton <drayton.kittel@gmail.com>
2026-01-31 22:10:23 -0600

    allow egress on http and https for initial instance start up and image download

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -32,6 +32,32 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   to_port           = 80
 }
 
+resource "aws_vpc_security_group_egress_rule" "allow_http_out" {
+    for_each = {
+    "frontend" = aws_security_group.sg-frontend.id,
+    "backend"  = aws_security_group.sg-backend.id
+  }
+  
+  security_group_id = each.value
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+resource "aws_vpc_security_group_egress_rule" "allow_https_out" {
+   for_each = {
+    "frontend" = aws_security_group.sg-frontend.id,
+    "backend"  = aws_security_group.sg-backend.id
+  }
+  
+   security_group_id = each.value
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443 
+}
+
+
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.sg-bastion.id
   cidr_ipv4         = "0.0.0.0/0"
