c4389c7be248e30e70cdd1d1abf4a6ff486b43c5
Dr8ton <drayton.kittel@gmail.com>
2026-02-02 22:26:59 -0600

    updated ec2 ref to sg.

--- terraform/ec2-catalogue.tf
+++ terraform/ec2-catalogue.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "catalogue" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   key_name               = "craftista-key"
-  vpc_security_group_ids = [aws_security_group.sg-backend.id]
+  vpc_security_group_ids = [ aws_security_group.backend-sgs["catalogue"]]
   subnet_id              = aws_subnet.private.id
   user_data              = <<EOT
 #!/bin/bash
