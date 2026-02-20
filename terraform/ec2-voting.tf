279a25dad76a9ca297357f1584ed93a66848f265
Dr8ton <drayton.kittel@gmail.com>
2026-02-20 16:59:13 -0600

    updated SG id for dynamic rules

--- terraform/ec2-voting.tf
+++ terraform/ec2-voting.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "voting" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   key_name               = "craftista-key"
-  vpc_security_group_ids = [aws_security_group.backend-sgs["voting"]]
+  vpc_security_group_ids = [aws_security_group.backend-sgs["voting"].id]
   subnet_id              = aws_subnet.private.id
   user_data              = <<EOT
 #!/bin/bash
